abb9fdd4a0ef274d906a78ec9099d92300722461
binzeli <104751975+binzeli@users.noreply.github.com>
2024-05-30 17:24:23 -0700

    Update ActorCriticAlgorithm.py

--- ActorCriticAlgorithm.py
+++ ActorCriticAlgorithm.py
@@ -7,16 +7,14 @@ import torch.nn.functional as F
 import random
 
 class Actor(nn.Module):
-    def __init__(self, numAgentState, actionSize):
+    def __init__(self, numAgent, actionSize):
         super(Actor, self).__init__()
-        self.numAgentState = int(numAgentState)
+        self.numAgent = int(numAgent*2)
         self.actionSize = int(actionSize)
-        self.linear1 = nn.Linear(self.numAgentState, 128)
-        self.linear2 = nn.Linear(128, self.actionSize)
+        self.linear1 = nn.Linear(self.numAgent, 16)
+        self.linear2 = nn.Linear(16, self.actionSize)
 
     def forward(self, state):
-        #state = torch.from_numpy(state).float()  # Convert state to a Tensor
-        #state = torch.tensor(state, dtype=torch.float)  # Convert state to a Tensor
         output = F.relu(self.linear1(state))
         output = self.linear2(output)
         policy = F.softmax(output, dim=-1)
@@ -24,15 +22,13 @@ class Actor(nn.Module):
 
 
 class Critic(nn.Module):
-    def __init__(self, numAgentState):
+    def __init__(self, numAgent):
         super(Critic, self).__init__()
-        self.numAgentState = int(numAgentState)
-        self.linear1 = nn.Linear(self.numAgentState, 128)
-        self.linear2 = nn.Linear(128, 1)
+        self.numAgent = int(numAgent*2)
+        self.linear1 = nn.Linear(self.numAgent, 16)
+        self.linear2 = nn.Linear(16, 1)
 
     def forward(self, state):
-        #state = torch.from_numpy(state).float()  # Convert state to a Tensor
-        #state = torch.tensor(state, dtype=torch.float)  # Convert state to a Tensor
         output = F.relu(self.linear1(state))
         value = self.linear2(output)
         return value
@@ -49,105 +45,84 @@ class ActorCriticAlgorithm():
         self.minEpsilon = 0.01
         self.maxEpsilon = 1
         self.decayRate = 0.001
-        self.gamma = 0.6
+        self.gamma = 0.99
         self.policy = None
-        self.actionProbList = []
-        self.criticList = []
-        self.rewardList = []
+        self.actionProb = None
 
+
+    def getCritic(self, state):
+        return self.critic.forward(state)
+
+        
+    def getPolicy(self, states):
+        output = self.actor(states)
+        return output
+    
 
     def updateEpsilon(self, episode):
         if episode % 100 == 0: 
             self.epsilon = self.minEpsilon + (self.maxEpsilon - self.minEpsilon) * np.exp(-self.decayRate * episode)
 
-        
-    def getPolicy(self, state):
-        ## input: a state in the form of a tensor
-        ## output: a policy in tensor form
-        return self.actor.forward((state))    # an tensor of length of action space
-    
 
-    def getExploredOrExploitedAction(self, episode):
-        ## output: a single action
     
+    def exploreOrExploit(self, episode):
         self.updateEpsilon(episode)
         if np.random.random() < self.epsilon:
-            action = torch.distributions.Categorical(self.policy).sample().item()
+            action_tensor = torch.distributions.Categorical(self.policy).sample()
         else:
-            action = torch.argmax(self.policy).item()
-        return action, self.policy[action]
+            action_tensor = torch.argmax(self.policy, dim=-1, keepdim=True)  # Keep as tensor for indexing
 
-    
+        action_prob = torch.gather(self.policy, -1, action_tensor).squeeze(-1)  # Extract and remove extra dimensions
+        return  action_tensor.item() , action_prob
+
 
     def chooseAction(self, state, agentId, episode):
-        if not isinstance(state, torch.Tensor):
-            state = torch.from_numpy(state).float()  # Convert state to a Tensor
+        state = torch.tensor(state, requires_grad=True, dtype=torch.float).reshape(-1)
         self.policy = self.getPolicy(state)
-        action, actionProb = self.getExploredOrExploitedAction(episode)
-        self.actionProbList.append(actionProb)
+        action, actionProb = self.exploreOrExploit(episode)
+        self.actionProb = actionProb
         return action
-
-
-
-    def getCritic(self, state):
-        ## input: a state in the form of a tensor
-        ## output: a value in tensor form
-        return self.critic.forward((state))     
-
-
-    def normalizeReward(self):
-        self.rewardList = torch.tensor(self.rewardList, dtype=torch.float)  # Convert rewardList to a Tensor
-        returns = torch.zeros_like(self.rewardList)
-        discountedSum = 0.0
-        for i in reversed(range(len(self.rewardList))):
-                discountedSum = self.rewardList[i] + self.gamma * discountedSum
-                returns[i] = discountedSum
-                returns = (returns - returns.mean()) / (returns.std() + 1e-8)
-        self.rewardList = returns
     
 
-
-    def getAdvantage(self):
-        self.normalizeReward()
-        return self.rewardList - self.criticList
+    def getAdvantage(self, currentValue,nextValue,reward):
+        return reward + self.gamma * nextValue - currentValue
 
 
     def updateActor(self, advantage):
         self.optimizerActor.zero_grad()
-        self.actionProbList = torch.stack(tuple(self.actionProbList), 0)  # Convert actionProbList to a Tensor
-        actorLoss = -(torch.sum(torch.log(self.actionProbList) * advantage))
-        actorLoss.backward(retain_graph=True)
+        actorLoss = -self.actionProb * advantage
+        actorLoss.backward(retain_graph=True )
         self.optimizerActor.step()
+        
 
 
-    def updateCritic(self):
+    def updateCritic(self, reward, critic):
         self.optimizerCritic.zero_grad()
-        self.criticList = torch.stack(tuple(self.criticList), 0) 
-        self.rewardList = torch.stack(tuple(self.rewardList), 0)  
-        lossFunction = torch.nn.SmoothL1Loss()  
-        criticLoss = lossFunction(self.rewardList.view(-1), self.criticList.view(-1)) 
-        criticLoss.backward() 
+        criticLoss = F.mse_loss(reward, critic)
+        criticLoss.backward()
         self.optimizerCritic.step()
         
         
-
-
-    def updateActorCritic(self):
-        advantage = self.getAdvantage(self.criticList, self.rewardList)
+    def updateActorCritic(self, currentValue,nextValue,reward):
+        advantage = self.getAdvantage(currentValue,nextValue,reward)
         self.updateActor(advantage)
-        self.updateCritic()
+        self.updateCritic(reward, currentValue)
+        
 
     
-
-
-        
-  
     def learn(self, allAgentCurrentStates, action, allAgentNextStates, reward):
-        if not isinstance(allAgentCurrentStates, torch.Tensor):
-            allAgentCurrentStates = torch.from_numpy(allAgentCurrentStates).float()  # Convert state to a Tensor
-        self.criticList.append(self.getCritic(allAgentCurrentStates))
-        self.rewardList.append(reward)
-        self.updateActorCritic()
+        numAgent = len(allAgentCurrentStates)
+        allAgentCurrentStates = torch.tensor(allAgentCurrentStates, requires_grad=True, dtype=torch.float).reshape(1, numAgent*2)
+        allAgentNextStates = torch.tensor(allAgentNextStates, requires_grad=True, dtype=torch.float).reshape(1, numAgent*2)
+        currentValue = self.getCritic(allAgentCurrentStates)
+        nextValue = self.getCritic(allAgentNextStates)
+        reward = torch.tensor(reward, dtype=torch.float, requires_grad=True)
+        self.updateActorCritic(currentValue,nextValue,reward)
+
+
+    def clearMemory(self):
+        self.actionProb = None 
+
 
 
         
@@ -156,20 +131,16 @@ class ActorCriticAlgorithm():
 
 
 class Agent:
-    def __init__(self, size, color, algorithm):
+    def __init__(self, size, color, agentId, algorithm):
         self.size = size
         self.color = color  
         self.algorithm = algorithm
+        self.agentId = agentId
+        self.state = None
+        self.reward = None
+        self.actionSpace = None
         
 
-
-        
-
-    def getSingleMaxRewardAction(self):
-        if not isinstance(self.state, torch.Tensor):
-            self.state = torch.from_numpy(self.state).float()  # Convert state to a Tensor
-        self.policy = self.algorithm.getPolicy(self.state)
-        self.action = torch.argmax(self.policy).item()
 
 
     
